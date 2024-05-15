b94dd3597f85b4b78e71022bb5b4934ee39e2eab
Asad <muhammadasaddun@gmail.com>
2024-05-15 16:10:05 +0700

    add Laravel 11

---
+++ laravel11/resources/views/blog.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel11 Blog</title>
+</head>
+<body>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat cupiditate aut non minus quos eos harum officiis dignissimos nam a optio odit vero, culpa perspiciatis obcaecati praesentium excepturi nesciunt!</p>
+    </article>
+    <article>
+        <h3>Judul 2</h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet earum illo, adipisci non provident suscipit deserunt similique accusamus obcaecati nisi atque, nostrum sapiente mollitia modi? Alias iure doloremque nihil repellendus.
+    </article>
+</body>
+</html>
